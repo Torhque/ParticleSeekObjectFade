b743658014a8d721e429bcb8e1dfb2d554eb53dd
Torhque <Torhque@live.com>
2017-02-16 00:58:07 -0700

    Add files via upload

---
+++ EffectFadeOut.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectFadeOut : MonoBehaviour 
+{
+	[SerializeField]
+	float fadeDuration = 3.0f;
+	[SerializeField]
+	bool startFade = false;
+
+	void Start () 
+	{
+		if (startFade) {
+			PerformFadeOut ();
+		}
+	}
+
+	void OnValidate() {
+		if (startFade) {
+			PerformFadeOut ();
+		}
+	}
+
+	public void PerformFadeOut() {
+		StartCoroutine (FadeOutEffect (this.transform, 0, true, fadeDuration));
+	}
+
+	// MATERIAL ON THE OBJECT MUST BE SET TO TRANSPARENT
+	private IEnumerator FadeOutEffect (Transform tf, float targetAlpha, bool isVanish, float duration)
+	{
+		// Renderer variable for accessing our object's Renderer component.
+		Renderer renderer = tf.GetComponent<Renderer> ();
+
+		// Float variable for storing the difference in the object's alpha values
+		float diffAlpha = (targetAlpha - renderer.material.color.a);
+
+		float counter = 0; // Counter for keeping track of deltaTime
+
+		// While the counter is less than the desired duration
+		while (counter < duration) 
+		{
+			// Variable for storing the object's alpha amount & difference in alpha divided by the duration
+			float alphaAmount = renderer.material.color.a + (Time.deltaTime * diffAlpha) / duration;
+
+			// Set the object's renderer to the new amounts
+			renderer.material.color = new Color (renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, alphaAmount);
+
+			counter += Time.deltaTime; // Keep track of deltaTime that is passing
+			yield return new WaitForEndOfFrame();
+//			yield return null; // Alternate return variable (ignore for now)
+		}
+		// Set the object's renderer to the resulting amounts (this portrays the fading effect since it is updating the values)
+		renderer.material.color = new Color (renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, targetAlpha);
+
+		// Destroy the object as it vanishes
+		if (isVanish) 
+		{
+//			renderer.transform.gameObject.SetActive (false); // Alternate method that just disables the object
+			Destroy(renderer.transform.gameObject);
+		}
+	}
+}
---
+++ EffectParticleSeeking.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EffectParticleSeeking : MonoBehaviour 
+{
+	/// <summary>
+	/// 
+	///  	The explicit particle lifespan is actually the Duration field in the Inspector on the particle prefab itself.
+	/// 	The following particleLifespan variable will determine the lifespan you [the designer] want. 
+	/// 	However, if the particleLifespan below surpasses the 60 seconds that I have set, then you will have to update the 
+	/// 	Duration field mentioned earlier.
+	/// 
+	/// </summary>
+	public float particleLifespan;
+	public float particleSpeed; // For editing the speed of the particles
+
+	private ParticleSystem particleSystem; // For storing the particle system
+	private ParticleSystem.Particle[] particles; // Array for storing the particles being created by the particle system
+	[SerializeField]
+	private GameObject particleAttractor; // Gameobject for storing the object that will attract the particles
+	[SerializeField]
+	private ParticleSystem psPrefab; // For storing the particle prefab
+	private Transform targetTf; // For storing the target object's transform
+	private bool seeking = false;
+	[SerializeField]
+	UnityEvent onStartParticles;
+	[SerializeField]
+	UnityEvent onStopParticles;
+
+	void Start()
+	{
+		/// TEST Finds my placeholder Player reference in the scene
+		particleAttractor = (GameObject.Find ("HeroForm"));
+
+		targetTf = particleAttractor.GetComponent<Transform> ();
+	}
+
+	public void PerformParticleSeeking () 
+	{
+		if (!seeking) 
+		{
+			// this.targetTf is this class' variable while the targetTf is the paramater above
+			this.targetTf = targetTf; 
+
+			StartCoroutine (SendParticles (targetTf)); // Kick off the coroutine
+			seeking = true; // Coroutine is running
+		}
+	}
+
+	private IEnumerator SendParticles(Transform targetPos)
+	{
+		bool running = true;
+
+		// Instantiate the particle system at the [this] object's location. 
+		particleSystem = Instantiate (psPrefab, this.transform.position, this.transform.rotation) as ParticleSystem;
+
+		// Set array size equal to the total amount of particles being created
+		particles = new ParticleSystem.Particle[particleSystem.main.maxParticles];
+
+		onStartParticles.Invoke (); // Invoke an event once the particle system starts emitting
+
+		while (running) 
+		{
+			// Variable for storing all of the particles being created
+			int numParticlesAlive = particleSystem.GetParticles (particles);
+
+			// For each particle in the array
+			for (int i = 0; i < numParticlesAlive; i++) 
+			{
+				// Lerp the particle from its source to [this] object
+				particles [i].position = Vector3.Lerp (particles [i].position, targetPos.position, Time.deltaTime * particleSpeed);
+			}
+			// Fill the particles array with particles that have been created
+			particleSystem.SetParticles (particles, numParticlesAlive);
+
+			// For Debugging
+//			 Debug.Log (particleSystem.time);
+
+			// Stop the particle system once it reaches its lifespan (-1 because particleSystem.time doesn't always reach the exact value)
+			// The -1 also cuts the emitter off at just the right time to provide a cool effect
+			if (particleSystem.time >= (particleLifespan - 1)) 
+			{
+				particleSystem.Stop ();
+				onStopParticles.Invoke (); // Invoke an event when the particle system stops emitting
+				running = false; // Set running to false to escape this hell
+			}
+			yield return new WaitForEndOfFrame ();
+		}
+		seeking = false; // Coroutine is no longer running
+	}
+}
